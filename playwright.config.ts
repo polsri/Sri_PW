78f6007a231c2c999255e3f3981bb40d5384c8f0
Sri <sridevi.sri9986@gmail.com>
2026-06-27 21:55:10 +0530

    Added YML file

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   /* Retry on CI only */
  // retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
- // workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["list"],
@@ -34,7 +34,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: "only-on-failure",
     video: "retain-on-failure",
-    headless: false
+    headless: !process.env.CI  ? false : true
   },
 
   /* Configure projects for major browsers */
